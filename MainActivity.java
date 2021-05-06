4bda3a132fa1e36004453fc1a9c36725a6e1c76d
Jyotsna-Shetty <81788169+Jyotsna-Shetty@users.noreply.github.com>
2021-05-06 18:37:10 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -27,50 +27,53 @@ import java.nio.CharBuffer;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+   
     private ArrayList<String> task;
     private ArrayAdapter<String> taskAdapter;
     
-    //    private void showList() throws IOException {
-    //
-    //       //File listFile = new File(filesDir, "list.txt");
-    //        try {
-    //            FileInputStream showData = new FileInputStream("listFile.ser");
-    //            ObjectInputStream showStream = new ObjectInputStream(showData);
-    //            showStream.close();
-    //            System.out.println(task.toString());
-    //        } catch (IOException e) {
-    //            e.printStackTrace();
-    //        }
-
-
+    /* Code for displaying the tasks using files, commented out as it did not work.
+        private void showList() throws IOException {
+        File listFile = new File(filesDir, "list.txt");
+              try {
+                  FileInputStream showData = new FileInputStream("listFile.ser");
+                  ObjectInputStream showStream = new ObjectInputStream(showData);
+                  showStream.close();
+                  System.out.println(task.toString());
+              } catch (IOException e) {
+                  e.printStackTrace();
+              } */
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         ListView listView = (ListView) findViewById(R.id.listView);
-
         task = new ArrayList<String>();
-        /*try {
+        
+        /* Calling method to display tasks on screen, commented out as it did not work, kept for reference
+        try {
             showList();
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+        
+        // Displaying the newly added tasks on the app screen
         taskAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,task);
         listView.setAdapter(taskAdapter);
     }
 
-    public void onAddItem(View view) throws IOException {
+    // Click listener for ADD ITEM button
+    public void onAddItem(View view) throws IOException {            // IOException added for using files
         EditText NewItem= (EditText) findViewById(R.id.NewItem);
         task.add(NewItem.getText().toString());
-        //taskAdapter.add(NewItem.getText().toString());
-        NewItem.setText(" ");
+        NewItem.setText(" ");                                        // Resets text in the EditText box after every click
         taskAdapter.notifyDataSetChanged();
+        //taskAdapter.add(NewItem.getText().toString());                Alternate method to add new task to the Array Adapter
 
-
-        /*File file = new File(MainActivity.this.getFilesDir(), "text");
+        /* Code for writing newly added task(s) to the file, commented out as it did not work, kept for reference
+        File file = new File(MainActivity.this.getFilesDir(), "text");
         try {
             File listFile = new File(file, "list" );
             FileWriter saveData = new FileWriter(listFile);
